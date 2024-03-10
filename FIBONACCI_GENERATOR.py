27829f16c803b0e605174a6017c6c125a1231059
shoaibkorai <realshoaibkorai@gmail.com>
2024-03-10 22:04:03 +0500

    added the source code

--- FIBONACCI_GENERATOR.py
+++ FIBONACCI_GENERATOR.py
@@ -1,9 +1,12 @@
+#method to generate fibonacci sequence  by taking one arguement as (n) and generate Fibonacci numbers  til the length of the sequence reaches n.
 def fibonacci(n):
     fibonacci_sequence = [0, 1]
     while len(fibonacci_sequence) < n:
         fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
     return fibonacci_sequence[:n]
 
-# Example usage:
+
+
+#usage 
 fib_numbers = fibonacci(100)
 print(fib_numbers)
